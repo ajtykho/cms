4d0ed70bbf7d665be91fc8336704ca9ea6568092
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2020-10-10 01:01:15 -0600

    implemented document item and list SO PROUD :)

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -8,8 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class DocumentListComponent implements OnInit {
 
   constructor() { }
+  
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('1', 'Ali Tykhomyrova', 'Student in class', 'https', null),
+    new Document('2', 'Ivan Tykhomyrov', 'Husband of student', 'https', null),
+    new Document('3', 'Thea Tykhomyrova', 'Child of student', 'https', null),
+    new Document('4', 'Cergei Tykhomyrov', 'In-Law of student', 'https', null),
+    new Document('5', 'Yulia Tykhomyrova', 'In-Law of student', 'https', null),
+
+  ]
+
+
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.onSelectedDocument.emit(document);
   }
 
 }
